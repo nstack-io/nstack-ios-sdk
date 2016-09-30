d27ac024f9865b49d7776dee8c91fb3ffc8d1c1b
Todor <tobr@nodesagency.com>
2016-09-30 16:22:09 +0100

    New tvOS target

---
+++ NStackSDK-tvOS/NStackSDK-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  NStackSDK-tvOS.h
+//  NStackSDK-tvOS
+//
+//  Created by Todor Brachkov on 30/09/2016.
+//  Copyright © 2016 Nodes ApS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NStackSDK-tvOS.
+FOUNDATION_EXPORT double NStackSDK_tvOSVersionNumber;
+
+//! Project version string for NStackSDK-tvOS.
+FOUNDATION_EXPORT const unsigned char NStackSDK_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NStackSDK_tvOS/PublicHeader.h>
+
+
