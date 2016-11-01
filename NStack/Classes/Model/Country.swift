a5333fb845ab233a5f2e177d54efb79401183017
Chris Combs <chco@nodes.dk>
2016-11-01 12:08:09 +0100

    updating Country model

--- NStack/Classes/Model/Country.swift
+++ NStack/Classes/Model/Country.swift
@@ -22,49 +22,49 @@ public struct Country {
 	public var currency = ""
 	public var currencyName = ""
 	public var languages = ""
-	public var image: NSURL?
-	public var image2: NSURL? //<-image_2
-	public var timezone = Timezone()
+	public var imageUrl: NSURL?
+	public var imageUrl2: NSURL? //<- image_url_2
+	public var capitalTimeZone = Timezone()
 }
 
 extension Country: Serializable {
 	public init(dictionary: NSDictionary?) {
-		id           <== (self, dictionary, "id")
-		name         <== (self, dictionary, "name")
-		code         <== (self, dictionary, "code")
-		codeIso      <== (self, dictionary, "code_iso")
-		native       <== (self, dictionary, "native")
-		phone        <== (self, dictionary, "phone")
-		continent    <== (self, dictionary, "continent")
-		capital      <== (self, dictionary, "capital")
-		capitalLat   <== (self, dictionary, "capital_lat")
-		capitalLng   <== (self, dictionary, "capital_lng")
-		currency     <== (self, dictionary, "currency")
-		currencyName <== (self, dictionary, "currency_name")
-		languages    <== (self, dictionary, "languages")
-		image        <== (self, dictionary, "image")
-		image2       <== (self, dictionary, "image_2")
-		timezone     <== (self, dictionary, "timezone")
+		id              <== (self, dictionary, "id")
+		name            <== (self, dictionary, "name")
+		code            <== (self, dictionary, "code")
+		codeIso         <== (self, dictionary, "code_iso")
+		native          <== (self, dictionary, "native")
+		phone           <== (self, dictionary, "phone")
+		continent       <== (self, dictionary, "continent")
+		capital         <== (self, dictionary, "capital")
+		capitalLat      <== (self, dictionary, "capital_lat")
+		capitalLng      <== (self, dictionary, "capital_lng")
+		currency        <== (self, dictionary, "currency")
+		currencyName    <== (self, dictionary, "currency_name")
+		languages       <== (self, dictionary, "languages")
+		imageUrl        <== (self, dictionary, "image_url")
+		imageUrl2       <== (self, dictionary, "image_url_2")
+		capitalTimeZone <== (self, dictionary, "capital_time_zone")
 	}
 	
 	public func encodableRepresentation() -> NSCoding {
 		let dict = NSMutableDictionary()
-		(dict, "id")            <== id
-		(dict, "name")          <== name
-		(dict, "code")          <== code
-		(dict, "code_iso")      <== codeIso
-		(dict, "native")        <== native
-		(dict, "phone")         <== phone
-		(dict, "continent")     <== continent
-		(dict, "capital")       <== capital
-		(dict, "capital_lat")   <== capitalLat
-		(dict, "capital_lng")   <== capitalLng
-		(dict, "currency")      <== currency
-		(dict, "currency_name") <== currencyName
-		(dict, "languages")     <== languages
-		(dict, "image")         <== image
-		(dict, "image_2")       <== image2
-		(dict, "timezone")      <== timezone
+		(dict, "id")                <== id
+		(dict, "name")              <== name
+		(dict, "code")              <== code
+		(dict, "code_iso")          <== codeIso
+		(dict, "native")            <== native
+		(dict, "phone")             <== phone
+		(dict, "continent")         <== continent
+		(dict, "capital")           <== capital
+		(dict, "capital_lat")       <== capitalLat
+		(dict, "capital_lng")       <== capitalLng
+		(dict, "currency")          <== currency
+		(dict, "currency_name")     <== currencyName
+		(dict, "languages")         <== languages
+		(dict, "image_url")         <== imageUrl
+		(dict, "image_url_2")       <== imageUrl2
+		(dict, "capital_time_zone") <== capitalTimeZone
 		return dict
 	}
 }
