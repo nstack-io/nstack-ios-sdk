1ccf760e2f3e7cd2e5b95c0f612a93346ea68217
cglarsen <chgr@nodes.dk>
2017-11-14 14:50:08 +0100

    Added more test of content feature

--- NStackSDK/NStackSDKTests/NStackTests.swift
+++ NStackSDK/NStackSDKTests/NStackTests.swift
@@ -88,40 +88,111 @@ class NStackTests: XCTestCase {
     
     // MARK: - Content
     
-    func testContentResponse() {
+    func testContentResponseId() {
         let exp = expectation(description: "Content recieved")
-        NStack.sharedInstance.getContentResponseForId(60) { (response, error) in
+        NStack.sharedInstance.getContentResponse(60) { (response, error) in
             //parse content
             guard let dictionary = response as? NSDictionary,
-                let data = dictionary.object(forKey: "data") as? NSDictionary,
-                let name = data.object(forKey: "name") as? String,
+                let name = dictionary.object(forKey: "name") as? String,
                 name == "Africa" else {
                 XCTFail()
                 return
             }
+            print("Africa: \(dictionary)")
             exp.fulfill()
         }
         waitForExpectations(timeout: 5.0)
     }
     
-//    func testContentInvalidId() {
-//        let exp = expectation(description: "Invalid id")
-//        NStack.sharedInstance.getContentResponseForId(6029035) { (response, error) in
-//            //parse content
-//            if error == nil {
-//                XCTFail()
-//            } else {
-//                exp.fulfill()
-//            }
-//        }
-//        waitForExpectations(timeout: 5.0)
-//    }
-    
-    func testContentValueResponse() {
+    func testContentResponseSlug() {
         let exp = expectation(description: "Content recieved")
-        NStack.sharedInstance.getValueForId(60, andKey: "name") { (response, error) in
-            
+        NStack.sharedInstance.getContentResponse("africacontinent") { (response, error) in
+            //parse content
+            guard let dictionary = response as? NSDictionary,
+                let name = dictionary.object(forKey: "name") as? String,
+                name == "Africa" else {
+                    XCTFail()
+                    return
+            }
+            print("Africa: \(dictionary)")
+            exp.fulfill()
+        }
+        waitForExpectations(timeout: 5.0)
+    }
+    
+    func testContentResponseExtended() {
+        let exp = expectation(description: "Content recieved")
+        NStack.sharedInstance.getContentResponse(60, key: "name") { (response, error) in
             if let name = response as? String, name == "Africa" {
+                print("Name is \(name)")
+                exp.fulfill()
+            } else {
+                XCTFail()
+            }
+        }
+        waitForExpectations(timeout: 5.0)
+    }
+    
+    func testContentResponseWrongUnwrapper() {
+        let exp = expectation(description: "Content recieved")
+        NStack.sharedInstance.getContentResponse(60, { $0.0["noDataHere"]}) { (response, error) in
+            if let error = error as? NStackError.Manager {
+                switch error {
+                case .parsing(let reason):
+                    if reason == "No data found using the default or specified unwrapper" {
+                        exp.fulfill()
+                    } else {
+                        XCTFail()
+                    }
+                default:
+                    XCTFail()
+                }
+            } else {
+                XCTFail()
+            }
+        }
+        waitForExpectations(timeout: 5.0)
+    }
+    
+    func testContentResponseWrongKey() {
+        let exp = expectation(description: "Content recieved")
+        NStack.sharedInstance.getContentResponse(60, key: "noDataForKey") { (response, error) in
+            if let error = error as? NStackError.Manager {
+                switch error {
+                case .parsing(let reason):
+                    if reason == "No data found for specified key" {
+                        exp.fulfill()
+                    } else {
+                        XCTFail()
+                    }
+                default:
+                    XCTFail()
+                }
+            } else {
+                XCTFail()
+            }
+        }
+        waitForExpectations(timeout: 5.0)
+    }
+    
+    func testContentInvalidId() {
+        let exp = expectation(description: "Invalid id")
+        NStack.sharedInstance.getContentResponse(6029035) { (response, error) in
+            //parse content
+            if error != nil {
+                exp.fulfill()
+            } else {
+                XCTFail()
+            }
+        }
+        waitForExpectations(timeout: 5.0)
+    }
+    
+    func testContentInvalidSlug() {
+        let exp = expectation(description: "Invalid slug")
+        NStack.sharedInstance.getContentResponse("invalidSlug") { (response, error) in
+            //parse content
+            if error != nil {
                 exp.fulfill()
             } else {
                 XCTFail()
