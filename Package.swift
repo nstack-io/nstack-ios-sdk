427573b5525ecb0ba688b49c667dfdd66c160c11
Jakob Mygind <jakob.mygind@ufst.dk>
2020-09-22 20:24:50 +0200

    change name

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "NStackSDK-iOS",
+            name: "NStackSDK",
             targets: ["NStackSDK-iOS"]),
     ],
     dependencies: [
