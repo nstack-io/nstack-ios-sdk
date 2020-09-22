a3b5a5a4199617766fd944d2a86333eb1ae10780
Jakob Mygind <jakob.mygind@ufst.dk>
2020-09-22 20:31:16 +0200

    rename

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NStackSDK",
-            targets: ["NStackSDK-iOS"]),
+            targets: ["NStackSDK"]),
     ],
     dependencies: [
         
@@ -31,7 +31,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "NStackSDK-iOS",
+            name: "NStackSDK",
             dependencies: [
                 .product(name: "LocalizationManager", package: "LocalizationManager", condition: .when(platforms: [.iOS])),
 //                "LocalizationManager",
@@ -44,7 +44,7 @@ let package = Package(
             ),
         .testTarget(
             name: "NStackSDK Tests",
-            dependencies: ["NStackSDK-iOS"],
+            dependencies: ["NStackSDK"],
             path: "NStackSDKTests"
             ),
     ]
