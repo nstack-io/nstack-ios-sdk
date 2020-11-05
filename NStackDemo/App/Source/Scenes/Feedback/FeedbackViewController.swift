c3e344ab6a0be1c92bcb3efbf2e0cb8610d41b76
Jigna Patel <jipa@nodesagency.com>
2020-11-05 09:29:32 +0100

    FeedbackViewController design and added extension to show/hide UIView(pickerView)

--- NStackDemo/App/Source/Scenes/Feedback/FeedbackViewController.swift
+++ NStackDemo/App/Source/Scenes/Feedback/FeedbackViewController.swift
@@ -6,24 +6,155 @@
 //
 
 import UIKit
+import NStackSDK
+import LocalizationManager
 
 class FeedbackViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet weak var mainView: UIView!
+
+    @IBOutlet weak var feedbackTypeView: UIView! {
+        didSet {
+            feedbackTypeView.layer.borderWidth = 0.25
+            feedbackTypeView.layer.borderColor = UIColor.lightGray.cgColor
+            feedbackTypeView.layer.cornerRadius = 8.0
+        }
+    }
+
+    @IBOutlet weak var feedbackTypeLabel: UILabel!
+
+    @IBOutlet weak var nameTextField: UITextField! {
+        didSet {
+            nameTextField.placeholder = "_Enter name"
+        }
+    }
+
+    @IBOutlet weak var emailTextField: UITextField! {
+        didSet {
+            emailTextField.placeholder = "_Enter email"
+        }
+    }
+
+    @IBOutlet weak var messageTextView: UITextView! {
+        didSet {
+            messageTextView.layer.borderWidth = 0.5
+            messageTextView.layer.borderColor = UIColor.systemGray.cgColor
+        }
+    }
+
+    @IBOutlet weak var selectImageView: UIView! {
+        didSet {
+            selectImageView.layer.borderWidth = 0.25
+            selectImageView.layer.borderColor = UIColor.lightGray.cgColor
+            selectImageView.layer.cornerRadius = 8.0
+        }
+    }
+
+    @IBOutlet weak var selectImageButton: UIButton! {
+        didSet {
+            selectImageButton.setTitle("_select Image", for: .normal)
+        }
+    }
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBOutlet weak var submitButton: UIButton! {
+        didSet {
+            submitButton.setTitle("_Submit", for: .normal)
+            submitButton.backgroundColor = UIColor.systemBlue
+            submitButton.setTitleColor(.white, for: .normal)
+            submitButton.layer.cornerRadius = 8.0
+        }
+    }
+
+    @IBOutlet weak var pickerView: UIView!
+
+    @IBOutlet weak var feedbackTypePicker: UIPickerView! {
+        didSet {
+            feedbackTypePicker.dataSource = self
+            feedbackTypePicker.delegate = self
+        }
+    }
+
+    // MARK: - Properties
+    private var feedbackTypes: [FeedbackType] = [.bug, .feedback]
+    private var selectedFeedbackTypeIndex: Int?
+    private var imagePickerController = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.navigationItem.title = "_Feedback"
+        pickerView.isHidden = true
+
+        //Set initial value selected in UIPicker
+        selectedFeedbackTypeIndex = self.feedbackTypes.firstIndex(of: FeedbackType.bug) ?? 0
+        guard let selectedFeedbackTypeIndex = selectedFeedbackTypeIndex else { return }
+        self.feedbackTypePicker.selectRow(selectedFeedbackTypeIndex, inComponent: 0, animated: true)
+        feedbackTypeLabel.text = feedbackTypes[selectedFeedbackTypeIndex].rawValue
+    }
+
+    // MARK: - Callbacks -
+
+    @IBAction func pickerCancelButtonTapped(_ sender: Any) {
+        pickerView.hideView(mainViewHeight: mainView.frame.size.height)
+    }
+
+    @IBAction func pickerDoneButtonTapped(_ sender: Any) {
+        feedbackTypeLabel.text = feedbackTypes[selectedFeedbackTypeIndex ?? 0].rawValue
+        pickerView.hideView(mainViewHeight: mainView.frame.size.height)
+    }
+
+
+    @IBAction func selectFeedbackTypeButtonTapped(_ sender: Any) {
+        self.view.endEditing(true)
+        pickerView.showView(mainViewHeight: mainView.frame.size.height)
+    }
+
+    @IBAction func selectImageButtonTapped(_ sender: Any) {
+        self.view.endEditing(true)
+        imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.allowsEditing = true
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true)
+    }
+
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        self.view.endEditing(true)
+        //TODO - API call
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: - UIPickerViewDataSource, UIPickerViewDelegate
+extension FeedbackViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
 
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return feedbackTypes.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        selectedFeedbackTypeIndex = row
+        return feedbackTypes[row].rawValue
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension FeedbackViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image: UIImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            imageView.image = image
+        }
+        imagePickerController.dismiss(animated: true, completion: nil)
+    }
+
+    override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
+        imagePickerController.dismiss(animated: true)
+    }
 }
