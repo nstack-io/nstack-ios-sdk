7f6f14ff003fa67ad561680c2d81ef79fd8a9b42
Tiago Bras <tibr@nodesagency.com>
2021-01-27 12:19:19 +0000

    Remove unused setUp and tearDown from DateFormatterTests

--- NStackSDKTests/DateFormatterTests.swift
+++ NStackSDKTests/DateFormatterTests.swift
@@ -11,14 +11,6 @@ import XCTest
 
 class DateFormatterTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testISO8061DateFormatter() throws {
         let expectedDate = Date(timeIntervalSince1970: 1611749050)
         
