2f2cc3a1d623f7f9c8e861ef90679a90c5bcc61a
Andrew Lloyd <>
2022-01-13 17:15:32 +0000

    networking fix

--- NStackSDK/Classes/NStack Manager/ConnectionManager.swift
+++ NStackSDK/Classes/NStack Manager/ConnectionManager.swift
@@ -389,7 +389,7 @@ extension ConnectionManager {
 
         let url = baseURLv2 + "notify/rate_reminder_v2/events"
         let request = session.request(url, method: .post, parameters: params, headers: defaultHeaders)
-        session.startDataTask(with: request, completionHandler: completion)
+        session.startDataTask(with: request, convertFromSnakeCase: false, completionHandler: completion)
     }
     
     func checkToShowReviewPrompt(completion: @escaping Completion<RateReminderAlertModel>) {
@@ -398,7 +398,7 @@ extension ConnectionManager {
         ]
         let url = baseURLv2 + "notify/rate_reminder_v2"
         let request = session.request(url, method: .get, parameters: params, headers: defaultHeaders)
-        session.startDataTask(with: request, completionHandler: completion)
+        session.startDataTask(with: request, convertFromSnakeCase: false, completionHandler: completion)
     }
     
     func logReviewPromptResponse(reminderId: String,
